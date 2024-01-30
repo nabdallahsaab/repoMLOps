8b9d06d0cbebd73837315e560beace4fc1715bd4
nabdallahsaab <nadine.saab23@gmail.com>
2024-01-30 14:30:37 +0100

    adding streamlit file

---
+++ run.py
@@ -0,0 +1,45 @@
+### Import
+
+
+
+# Cach data @st.cache_data
+
+
+### Config
+
+
+### Title et markdown : st.title et st.markdown
+
+
+### Checkbox st.checkbox
+
+
+### Selectbox st.selectbox
+
+
+
+### Forms st.form, st.form_submit_button et st.select_slider
+
+
+
+### Columns st.columns
+
+
+
+### Graphique Histogramme avec matplotlib.pyplot, seaborn et st.pyplot
+
+
+### Image avec st.image
+
+
+### Graphique Histogramme px.histogram et st.plotly_chart
+
+
+
+### Graphique Pie chart px.pie et st.plotly_chart
+
+
+
+### + De graphiques ici : https://plotly.com/python/
+
+
